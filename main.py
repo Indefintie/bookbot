1ff246b55367d623ec3dd795153d9ffbf0c9a9f5
Indefinite <jeremy575757online@gmail.com>
2025-12-15 13:35:28 -0330

    C: prinst list of letters with formating

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from stats import get_book_text
-from stats import get_num_letters  
+from stats import get_num_letters 
+from stats import get_num_words
+from stats import order_dic 
 def main():
 	text = get_book_text("books/frankenstein.txt")
 	dic_out_put = get_num_letters(text)
-	print(dic_out_put)
+	num_of_words = get_num_words(text)
+	ordered_dicshon = order_dic(dic_out_put)
+	
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
+	print(f"Found {num_of_words} total words")
+	print("--------- Character Count -------")
+	for item in ordered_dicshon:
+		if item["char"].isalpha():
+			print(f"{item['char']}:{item['num']}")
+	print("============= END ===============")
+	
 	
 
 main()
--- stats.py
+++ stats.py
@@ -7,11 +7,29 @@ def get_num_words(text):
         num_words = len(word_list)
         return num_words 
 def get_num_letters(text):
-        letter_list = (",",".","?","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","@","#","$","%"," ","^","&","*","-","=","+")
-        lower_case_text = text.lower()
-        dictionary_count = {}
-        for letter in letter_list:
-                temp_letter_count = lower_case_text.count(letter)
-                dictionary_count[letter] = temp_letter_count
-        return dictionary_count
+        low_text =text.lower()
+        chars = list(low_text)
+        char_list = {}
+        for letter in chars:
+                if letter in char_list:
+                        char_list[letter] += 1
+                else :
+                        char_list[letter] = 1
+        return char_list
+
+def order_dic(dic_out_put):
+        list_dic=[]
+        for char, count in dic_out_put.items():
+                tmp_merg = {"char": char, "num": count}
+                list_dic.append(tmp_merg)
+        def sort_on(items):
+                return items["num"]
+        list_dic.sort(reverse=True, key=sort_on)
+       
+        return list_dic
+
+
+
+
+
 

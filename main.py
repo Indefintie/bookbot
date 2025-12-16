0ebee1b03900ca7d94de97349da323925655aa14
Indefinite <jeremy575757online@gmail.com>
2025-12-16 09:59:30 -0330

    D. take a console comand python3 ./main.py ./<book_path> and gives letters count, word cound,

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ from stats import get_book_text
 from stats import get_num_letters 
 from stats import get_num_words
 from stats import order_dic 
+import sys
 def main():
-	text = get_book_text("books/frankenstein.txt")
-	dic_out_put = get_num_letters(text)
-	num_of_words = get_num_words(text)
-	ordered_dicshon = order_dic(dic_out_put)
-	
-	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
-	print("----------- Word Count ----------")
-	print(f"Found {num_of_words} total words")
-	print("--------- Character Count -------")
-	for item in ordered_dicshon:
-		if item["char"].isalpha():
-			print(f"{item['char']}:{item['num']}")
-	print("============= END ===============")
-	
+	if len(sys.argv) == 2:
+		text = get_book_text(f"{sys.argv[1]}")
+		dic_out_put = get_num_letters(text)
+		num_of_words = get_num_words(text)
+		ordered_dicshon = order_dic(dic_out_put)
+		
+		print("============ BOOKBOT ============")
+		print(f"Analyzing book found at {sys.argv[1]}...")
+		print("----------- Word Count ----------")
+		print(f"Found {num_of_words} total words")
+		print("--------- Character Count -------")
+		for item in ordered_dicshon:
+			if item["char"].isalpha():
+				print(f"{item['char']}: {item['num']}")
+		print("============= END ===============")
+	else:
+		print("Usage: python3 main.py <path_to_book>")
+		return sys.exit(1)
 	
 
 main()
